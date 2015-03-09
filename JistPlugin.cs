0fede14b1995a47d3f6eeaaeed170a4cfad7039d
Tyler Watson <tyler@tw.id.au>
2015-03-09 16:10:45 +1000

    Jist Update 17

--- JistPlugin.cs
+++ JistPlugin.cs
@@ -8,7 +8,7 @@ using System.Reflection;
 using Wolfje.Plugins.Jist.Framework;
 
 namespace Wolfje.Plugins.Jist {
-	[ApiVersion(1, 16)]
+	[ApiVersion(1, 17)]
 	public class JistPlugin : TerrariaPlugin {
 		public static JistEngine Instance { get; protected set; }
 		public override string Author { get { return "Wolfje"; } }
